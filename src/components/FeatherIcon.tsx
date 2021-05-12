cae19e686a5e191c70164b4b2eb67b90791ac9de
rubyistdotjs <rubyistdotjs@gmail.com>
2021-05-12 16:43:20 +0200

    Cleanup

--- src/components/FeatherIcon.tsx
+++ src/components/FeatherIcon.tsx
@@ -1,11 +1,11 @@
-import React, { FC } from 'react';
+import React from 'react';
 import * as Icons from 'react-feather';
 
 export type FeatherIconName = keyof typeof Icons;
 
 export type FeatherIconProps = { name: FeatherIconName } & Icons.IconProps;
 
-export const FeatherIcon: FC<FeatherIconProps> = ({ name, ...iconProps }) => {
+export const FeatherIcon = ({ name, ...iconProps }: FeatherIconProps) => {
   const IconComponent = Icons[name];
   return <IconComponent {...iconProps} />;
 };
